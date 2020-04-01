8cfe2958477d50f6d49d77a35e82f50eafeeb7bf
fkinara <42002518+fkinara@users.noreply.github.com>
2020-04-01 17:49:51 +0700

    success

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,13 @@ Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@login');
 
 Route::middleware(['jwt.verify'])->group(function(){
-    Route::get('user', 'UserController@getAuthenticatedUser');
+    Route::delete('user/{id}', "UserController@delete"); 
+    Route::get('user/{id}', 'UserController@index'); 
+    Route::get('user/{limit}/{offset}', "UserController@getAll"); 
+	Route::post('logout', "UserController@logout"); 
+    
+    Route::get('dailyscrum/{id}', "DailyScrumController@index"); 
+    Route::get('dailyscrum/{limit}/{offset}', "DailyScrumController@getAll"); 
+	Route::post('dailyscrum', 'DailyScrumController@store'); 
+	Route::delete('dailyscrum/{id}', "DailyScrumController@delete"); 
 });
